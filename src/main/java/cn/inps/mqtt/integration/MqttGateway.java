c6b2c7f3746df3da4329f0dd4cd1ebb2174ed301
inps <fevany@163.com>
2020-09-27 17:10:03 +0800

    modify messaginggateway

--- src/main/java/cn/inps/mqtt/integration/MqttGateway.java
+++ src/main/java/cn/inps/mqtt/integration/MqttGateway.java
@@ -7,4 +7,7 @@ import org.springframework.messaging.handler.annotation.Header;
 @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
 public interface MqttGateway {
     void sendToMqtt(String data, @Header(MqttHeaders.TOPIC) String topic);
+    // 定义重载方法，用于消息发送
+    void sendToMqtt(String payload);
+    void sendToMqtt(@Header(MqttHeaders.TOPIC) String topic, @Header(MqttHeaders.QOS) int qos, String payload);
 }
